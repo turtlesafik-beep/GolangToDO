b81eb998d646fc13c9a8afc3cbdd18a2bf241a4f
Данила <Danila Saf>
2026-05-07 19:20:33 +0300

    create users

--- internal/features/users/service/create_user.go
+++ internal/features/users/service/create_user.go
@@ -2,6 +2,7 @@ package user_service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/turtlesafik-beep/GolangToDO/internal/core/domain"
 )
@@ -10,5 +11,14 @@ func (s *UsersService) CreateUser(
 	ctx context.Context,
 	user domain.User,
 ) (domain.User, error) {
+	if err := user.Validate(); err != nil {
+		return domain.User{}, fmt.Errorf("Validate user domain: %w", err)
+	}
 
+	user, err := s.CreateUser(ctx, user)
+	if err != nil {
+		return domain.User{}, fmt.Errorf("create user: %w", err)
+	}
+
+	return user, nil
 }
